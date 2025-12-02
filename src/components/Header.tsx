a1877cd521a478b61a1beb70efc308b6b40eeb8a
Gvantsa <gvantsaletta9@gmail.com>
2025-12-02 09:58:16 +0400

    unused import deleted

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import Notification from "@/components/Notification.tsx";
 import {SidebarTrigger} from "@/components/ui/sidebar.tsx";
 import {useUserAuth} from "@/store/auth/hooks/useUserAuth.ts";
 import SignOutButton from "@/components/SignOut.tsx";
-import {useEffect, useState} from "react";
+import {useEffect} from "react";
 
 
 const Header = () => {
